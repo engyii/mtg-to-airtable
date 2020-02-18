06a9646349824e0f3a6d86dd9d7a2222f6d573d3
Enguerrand Leclerc <e@en.gy>
2020-02-18 23:22:58 +0100

    Switch to scryfall api

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ Airtable.configure({
     apiKey: secret.apiKey
 })
 
-const fields = ['uuid', 'number', 'name','text', 'type', 'rarity', 'colors', 'colorIdentity', 'manaCost', 'power', 'toughness', 'scryfallIllustrationId', 'scryfallId']
+const scryfallFields = ['id', 'arena_id', 'set', 'collector_number', 'name', 'oracle_text', 'type_line', 'rarity','color_identity', 'colors', 'mana_cost', 'cmc', 'power', 'toughness' ]
+
 const base = Airtable.base(secret.db)
 
 const findRecord = async (table, filter) => {
@@ -29,12 +30,12 @@ const createOrUpdateRecord = async (record, table, primaryKey, doNotUpdate) => {
     const existingRecord = await findRecord(table, primaryKey + " = '" + record[primaryKey] + "'")
 
     if (!existingRecord) {
-        await base(table).create(record, {typecast: true}).catch(err => displayErrorMsgs(err, record))
+        await base(table).create(record, {typecast: true})
         console.log("Non-existing record '" + record[primaryKey] + "' Created")
     } else {
 
         if (!doNotUpdate) {
-            await base(table).replace(existingRecord.id, record, {typecast: true}).catch(err => displayErrorMsgs(err, record))
+            await base(table).replace(existingRecord.id, record, {typecast: true})
             console.log("Existing record '" + record[primaryKey] + "' Updated")
         } else {
             console.log("Existing record '" + record[primaryKey] + "' Untouched")
@@ -42,16 +43,21 @@ const createOrUpdateRecord = async (record, table, primaryKey, doNotUpdate) => {
     }
 
 }
-const importImage = async (scryfallId) => {
 
+const importTherosFromScryfall = async (season) => {
+    try {
+        let page = 'https://api.scryfall.com/cards/search?format=json&include_extras=false&include_multilingual=false&order=name&page=1&q=e%3Dthb&unique=cards'
+        do {
+            const { data : { data:cards, next_page} } = await axios.get(page)
+            //console.log({ ...(_.pick(cards[0], scryfallFields)), img: { url: cards[0].image_uris.large}, art_crop: { url: cards[0].image_uris.art_crop }, ...(_.pick(cards[0].related_uris, ['gatherer', 'mtgtop8', 'tcgplayer_decks'])) })
+            await Promise.all(cards.map(card => createOrUpdateRecord({ ...(_.pick(card, scryfallFields)), img: [{ url: card.image_uris.large}], art_crop: [{ url: card.image_uris.art_crop }], ...(_.pick(card.related_uris, ['gatherer', 'mtgtop8', 'tcgplayer_decks'])) } , 'Cards', 'id')))
+            page = next_page
+        } while(page)
+    } catch(e) {
+        console.log(e)
+    }
 }
 
-const importTheros = async (season) => {
-    const res = await axios.get('https://www.mtgjson.com/json/THB.json')
-    console.log(res.data.cards)
-    return await Promise.all(res.data.cards.map(card => createOrUpdateRecord(_.pick(card, fields) , 'THB', 'uuid')))
-}
-
-importTheros();
+importTherosFromScryfall();
 
 
